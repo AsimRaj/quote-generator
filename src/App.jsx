3cf0ecd0cb0dc056ff1a0be70b0c382397457430
AsimRaj <asimrajbangshi41@gmail.com>
2025-11-14 04:25:39 +0600

    Add fetch Real Quote From an API

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 // import './App.css'
-const quotes = [
-  {
-    text: "The best way to get started is to quit talking and begin doing.",
-    author: "Walt Disney",
-  },
-  {
-    text: "Don’t let yesterday take up too much of today.",
-    author: "Will Rogers",
-  },
-  {
-    text: "It’s not whether you get knocked down, it’s whether you get up.",
-    author: "Vince Lombardi",
-  },
-  {
-    text: "If you are working on something exciting, it will keep you motivated.",
-    author: "Steve Jobs",
-  },
-  {
-    text: "Success is not in what you have, but who you are.",
-    author: "Bo Bennett",
-  },
-];
 
 function App() {
-  const [quote, setQuote] = useState(quotes[0]);
-  const generatQuote = () => {
-    const randomQuote = Math.floor(Math.random() * quotes.length);
-    setQuote(quotes[randomQuote]);
+  const [quote, setQuote] = useState({ content: "", author: "" });
+  const [loading, setLoading] = useState(false);
+
+  const fetchQuote = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch("https://dummyjson.com/quotes/random");
+      const data = await res.json();
+      setQuote({ content: data.quote, author: data.author });
+
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching quotes:", error);
+      setLoading(false);
+    }
   };
+  //fetch quote when the app first load
+  useEffect(() => {
+    fetchQuote();
+  }, []);
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-200">
@@ -38,13 +31,20 @@ function App() {
         <h1 className="text-2xl font-bold text-gray-800 mb-4 ">
           Random Quote Generator
         </h1>
-        <p className="text-lg text-gray-700 italic mb-3 ">{quote.text}</p>
-        <p className="text-sm text-gray-500 mb-6">{quote.author}</p>
-        <button
-        onClick={generatQuote}
-         className="text-white bg-indigo-700 px-4 py-2 rounded-full hover:bg-indigo-900 transition">
-          New Quote
-        </button>
+        {loading ? (
+          <p className="text-gray-500 mb-6 italic">Loading...</p>
+        ) : ( 
+          <>
+            <p className="text-lg text-gray-700 italic mb-3 ">{quote.content}</p>
+            <p className="text-sm text-gray-500 mb-6">{quote.author}</p>
+            <button
+              onClick={fetchQuote}
+              className="text-white bg-indigo-700 px-4 py-2 rounded-full hover:bg-indigo-900 transition"
+            >
+              New Quote
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
