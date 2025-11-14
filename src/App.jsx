b2b52554afe6d1c60768bde71fc23453758e29d0
AsimRaj <asimrajbangshi41@gmail.com>
2025-11-14 17:51:08 +0600

    Add Animation like  Framer Motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import {motion, AnimatePresence} from 'framer-motion'
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 // import './App.css'
@@ -6,15 +7,19 @@ import viteLogo from "/vite.svg";
 function App() {
   const [quote, setQuote] = useState({ content: "", author: "" });
   const [loading, setLoading] = useState(false);
+  const [show, setShow] = useState(true)
 
   const fetchQuote = async () => {
     try {
       setLoading(true);
+      setShow(false);
       const res = await fetch("https://dummyjson.com/quotes/random");
       const data = await res.json();
-      setQuote({ content: data.quote, author: data.author });
-
+      setTimeout(() => {
+        setQuote({ content: data.quote, author: data.author });
+      setShow(true);
       setLoading(false);
+      }, 300);
     } catch (error) {
       console.error("Error fetching quotes:", error);
       setLoading(false);
@@ -31,20 +36,36 @@ function App() {
         <h1 className="text-2xl font-bold text-gray-800 mb-4 ">
           Random Quote Generator
         </h1>
-        {loading ? (
-          <p className="text-gray-500 mb-6 italic">Loading...</p>
-        ) : ( 
-          <>
+        <AnimatePresence mode="wait">
+          {loading ? (
+          <motion.p 
+          key="loading"
+          initial={{opacity:0}}
+          animate={{opacity: 1}}
+          exit={{opacity:0}}
+          className="text-gray-500 mb-6 italic">Loading...</motion.p>
+        ) : (
+          show && ( 
+          <motion.div
+          key={quote.content}
+          initial={{opacity:0,y:20}}
+          animate={{opacity:1, y:0}}
+          exit={{opacity:0 , y:-20}}
+          >
             <p className="text-lg text-gray-700 italic mb-3 ">{quote.content}</p>
             <p className="text-sm text-gray-500 mb-6">{quote.author}</p>
-            <button
+          </motion.div>
+            
+          
+        )
+        )}
+        </AnimatePresence>
+        <button
               onClick={fetchQuote}
               className="text-white bg-indigo-700 px-4 py-2 rounded-full hover:bg-indigo-900 transition"
             >
               New Quote
             </button>
-          </>
-        )}
       </div>
     </div>
   );
