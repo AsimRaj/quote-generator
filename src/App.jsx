55052386dfb12ff8314271dfd0fd176b2b172813
AsimRaj <asimrajbangshi41@gmail.com>
2025-11-16 05:22:20 +0600

    Add LocalStorage Persistance for Favorite Quotes(add and remove)

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,23 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [show, setShow] = useState(true);
   const [copied, setCopied] = useState(false);
+  const [favorites, setFavorites] = useState(()=>{
+    const saved = localStorage.getItem("favorites");
+    return saved ? JSON.parse(saved) : []
+  });
+
+  //load favorites from localStorage
+  // useEffect(() => {
+  //   const saved = JSON.parse(localStorage.getItem("favorites")) || [];
+  //   setFavorites(saved);
+  // }, []);
+
+  //Save favorites to localStorage
+  useEffect(() => {
+   
+      localStorage.setItem("favorites", JSON.stringify(favorites));
+    
+  }, [favorites]);
 
   //fetch a random quote from the API
   const fetchQuote = async () => {
@@ -33,33 +50,45 @@ function App() {
     fetchQuote();
   }, []);
   //Copied to Clipboard
-  const copyQuote = async()=>{
+  const copyQuote = async () => {
     const text = `"${quote.content} - ${quote.author}"`;
     await navigator.clipboard.writeText(text);
     setCopied(true);
     // hide toast after 1.5 second
-    setTimeout(()=> setCopied(false), 1500)
-  }
+    setTimeout(() => setCopied(false), 1500);
+  };
+  const addFavorites = () => {
+    const exists = favorites.some(
+      (q) => q.content === quote.content && q.author === quote.author
+    );
+    if (!exists) {
+      setFavorites([...favorites, quote]);
+    }
+  };
+  const removeFavorites = (index) => {
+    const updated = [...favorites];
+    updated.splice(index, 1);
+    setFavorites(updated);
+  };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-200">
-     {/* toast notification */}
+    <div className="flex flex-col   items-center min-h-screen py-20 bg-linear-to-r from-indigo-500 to-purple-600 relative">
+      {/* toast notification */}
       <AnimatePresence>
-
-        {copied &&(
-        <motion.div
-        initial={{opacity:0, y: -20}}
-        animate={{opacity:1, y: 0}}
-        exit={{opacity:0, y: -20}}
-        transition={{duration: 0.3}}
-        className="absolute top-5  bg-black text-white text-sm rounded-lg shadow-lg px-4 py-2"
-        >
-        🗒️Copied to Clipboard
-        </motion.div>
-      )}
+        {copied && (
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.3 }}
+            className="absolute top-5  bg-black text-white text-sm rounded-lg shadow-lg px-4 py-2"
+          >
+            🗒️Copied to Clipboard
+          </motion.div>
+        )}
       </AnimatePresence>
-      
-      <div className="bg-white rounded-lg shadow-2xl p-8 min-w-md w-96 text-center">
+
+      <div className="bg-white mb-10 rounded-lg shadow-2xl p-8 max-w-lg w-10/12 h-8/12 max-h-3/4 text-center">
         <h1 className="text-2xl font-bold text-gray-800 mb-4 ">
           Random Quote Generator
         </h1>
@@ -94,15 +123,48 @@ function App() {
         <div className="flex justify-center gap-3 mt-4">
           <button
             onClick={fetchQuote}
-            className="text-white bg-indigo-700 px-5 py-2 rounded-full hover:bg-indigo-900 transition"
+            className="text-white bg-sky-500 px-5 py-2 rounded-full hover:bg-sky-600 transition"
           >
             New Quote
           </button>
           <button
-          onClick={copyQuote} className="text-white bg-indigo-700 px-5 py-2 rounded-full hover:bg-indigo-900 transition">
+            onClick={copyQuote}
+            className="text-white bg-green-500 px-5 py-2 rounded-full hover:bg-green-600 transition"
+          >
             Copy
           </button>
+          <button
+            onClick={addFavorites}
+            className="text-white bg-yellow-500 px-5 py-2 rounded-full hover:bg-yellow-600 transition"
+          >
+            ⭐ Favorites
+          </button>
         </div>
+      </div>
+      {/* Favorites List */}
+      <div className="bg-white max-w-lg w-10/12 shadow-xl rounded-2xl px-6 py-4">
+        <h1 className="text-xl font-bold text-gray-700 mb-3">⭐ Favorites</h1>
+        {favorites.length === 0 ? (
+          <p className="text-sm text-gray-700">No Favorites yet.</p>
+        ) : (
+          favorites.map((fav, index) => (
+            <div
+              key={index}
+              className="flex justify-between items-center border-b py-2"
+            >
+              <div>
+                <p className="text-sm text-gray-700 italic">"{fav.content}"</p>
+                <p className="text-xs text-gray-500">--{fav.author}</p>
+              </div>
+              <button
+                onClick={() => removeFavorites(index)}
+                className="text-red-500 hover:underline text-sm"
+              >
+                remove
+              </button>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
